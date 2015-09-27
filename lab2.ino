e643170ca12439f41925b1f2897e64eb0facf906
Cindy Luu <cindyluu@dhcp-ccc-2684.eduroam.cornell.edu>
2015-09-27 13:55:09 -0400

    updated over weekend

--- lab2.ino
+++ lab2.ino
@@ -1,32 +1,40 @@
 
 //strobe, clock, data, output enable pins
 
-int datapin = 10;
-int strobepin = 11;
-int clockpin = 12;
-int outputen = 13;
+int button = 2;
+int strobepin = 3;
+int datapin = 4;
+int clockpin = 5;
+int oepin = 6;
 
 //column pins
-int col1 = 5;
-int col2 = 6;
-int col3 = 7;
-int col4 = 8;
-int col5 = 9;
+int col1 = 9;
+int col2 = 10;
+int col3 = 11;
+int col4 = 12;
+int col5 = 13;
 int columns[5] = {col1, col2, col3, col4, col5};
+
+//char zero[5] = {'0x00',  '0xFF',  '0x81',  '0x81',  '0xFF'};
+
 // on off definition
 #define col_off HIGH
 #define col_on LOW
  
 void setup() {
+pinMode(button,INPUT);
 pinMode(datapin,OUTPUT);
 pinMode(strobepin,OUTPUT);
 pinMode(clockpin,OUTPUT);
-pinMode(outputen,OUTPUT);
+pinMode(oepin,OUTPUT);
+
 pinMode(col1,OUTPUT);
 pinMode(col2,OUTPUT);
 pinMode(col3,OUTPUT);
 pinMode(col4,OUTPUT);
 pinMode(col5,OUTPUT);
+  Serial.begin(9600);
+  Serial.println("reset");
 }
 
 void columnsOFF() {
@@ -39,27 +47,43 @@ void columnsOFF() {
 
 
 void loop() {
+    digitalWrite(oepin,LOW);
     columnsOFF();
 // shift out the bits, take the latch pin high so the LEDs will light up, pause before next value:
 //1
+    digitalWrite(oepin,HIGH);
     digitalWrite(columns[3],col_on);
     digitalWrite(strobepin, LOW);
-    shiftOut(datapin, clockpin, MSBFIRST, 0xFF);  
+    shiftOut(datapin, clockpin, MSBFIRST, 0x4F);  
     digitalWrite(strobepin, HIGH);
     delay(1000);
-    columnsOFF();
-//4
-    for (int x = 0; x < 5; x++){
-        for(int y = 0; y <5; y++ ){
 
-        digitalWrite(columns[x],col_on);
-        if (x!=y) {
-        digitalWrite(columns[y],col_on);          
-        }    
-        digitalWrite(strobepin, LOW);
-        shiftOut(datapin, clockpin, MSBFIRST, 0x0F);  
-        digitalWrite(strobepin, HIGH);
-        delay(50);  
-      }    
-    }
+//2
+//    columnsOFF();
+//    digitalWrite(columns[0],col_on);
+//    digitalWrite(columns[1],col_on);
+//    digitalWrite(columns[2],col_on);
+//    digitalWrite(columns[3],col_on);
+//    digitalWrite(columns[4],col_on);
+//    digitalWrite(strobepin, LOW);
+//    shiftOut(datapin, clockpin, MSBFIRST, 0x4F);  
+//    digitalWrite(strobepin, HIGH);
+//    delay(500);
+
+
+//4
+//    columnsOFF();
+//    for (int x = 0; x < 5; x++){
+//        for(int y = 0; y <5; y++ ){
+//        digitalWrite(columns[x],col_on);
+//        if (x!=y) {
+//        digitalWrite(columns[y],col_off);          
+//        }    
+//        digitalWrite(strobepin, LOW);
+//        shiftOut(datapin, clockpin, MSBFIRST, 154);  
+//        delay(1); 
+//        digitalWrite(strobepin, HIGH);
+//        delay(50);  
+//      }    
+//    }
 }
